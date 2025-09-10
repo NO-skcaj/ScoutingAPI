6d5d4b4f9c03692bfbc8860e72fd0573d4544eb4
Vit <vit@vitly.xyz>
2025-09-10 17:36:04 -0400

    idk

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ source .venv/bin/activate
 echo "Upgrading pip and installing Flask..."
 pip install --upgrade pip
 pip install Flask
+pip install sqlite3
 
 echo "Operation Komplett!"
 echo "To start development, run: source .venv/bin/activate"
