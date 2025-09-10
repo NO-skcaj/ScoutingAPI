20806c575cda98586000436038f9b50eaefbb75a
Jackson Case <126780741+NO-skcaj@users.noreply.github.com>
2025-09-10 16:09:31 -0400

    Make vitaliy happy

---
+++ setup.py
@@ -0,0 +1,7 @@
+print("Creating a virtual environment for Flask.")
+python3 -m venv .venv
+print("Entering the virtual env.")
+. .venv/bin/activate
+print("Installing Flask")
+pip install Flask
+print("Operation Komplett!")
